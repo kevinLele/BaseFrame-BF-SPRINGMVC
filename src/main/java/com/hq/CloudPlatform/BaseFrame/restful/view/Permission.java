5344280d44f456fcb94bb7f83d71a7f534243013
kevinLele <kevin.zhan@foxmail.com>
2018-04-04 14:56:27 +0800

    init

---
+++ src/main/java/com/hq/CloudPlatform/BaseFrame/restful/view/Permission.java
@@ -0,0 +1,50 @@
+package com.hq.CloudPlatform.BaseFrame.restful.view;
+
+import com.hq.CloudPlatform.BaseFrame.entity.BaseEntity;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ * 权限属性表实体类
+ */
+public class Permission extends BaseEntity {
+
+    private String parentId;
+
+    private String name;
+
+    private String code;
+
+    private String type;
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

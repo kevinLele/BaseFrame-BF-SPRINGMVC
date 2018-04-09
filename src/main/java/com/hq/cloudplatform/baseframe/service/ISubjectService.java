2f3a90cd312aca909c6a3bbcced1c81083ecfd62
kevinLele <kevin.zhan@foxmail.com>
2018-04-09 14:33:51 +0800

    init

--- src/main/java/com/hq/cloudplatform/baseframe/service/ISubjectService.java
+++
@@ -1,9 +0,0 @@
-package com.hq.cloudplatform.baseframe.service;
-
-
-import com.hq.cloudplatform.baseframe.entity.Subject;
-
-public interface ISubjectService extends IBaseService<Subject> {
-
-    String getValue(String parentCode, String code);
-}

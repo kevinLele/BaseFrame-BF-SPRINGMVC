998ed3497b71c330b9bf2d502e4a90a42ad355e1
kevinLele <kevin.zhan@foxmail.com>
2018-04-12 14:22:33 +0800

    init

--- src/main/java/com/hq/cloudplatform/baseframe/sys/filter/CrossDomainFilter.java
+++ src/main/java/com/hq/cloudplatform/baseframe/sys/filter/CrossDomainFilter.java
@@ -6,17 +6,23 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author Administrator
+ */
 public class CrossDomainFilter extends AbstractFilter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse rsp = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
 
         //允许跨域的响应头设置
-        rsp.setHeader("Access-Control-Allow-Origin", "*");
+        rsp.setHeader("Access-Control-Allow-Credentials", "true");
+        rsp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
         rsp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         rsp.setHeader("Access-Control-Max-Age", "3600");
         rsp.setHeader("Access-Control-Allow-Headers", "Content-Type");

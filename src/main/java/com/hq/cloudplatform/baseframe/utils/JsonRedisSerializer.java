998b0f157de5761714d0e612b3d8196a1aece15a
kevinLele <kevin.zhan@foxmail.com>
2018-04-13 18:15:30 +0800

    init

--- src/main/java/com/hq/cloudplatform/baseframe/utils/JsonRedisSerializer.java
+++ src/main/java/com/hq/cloudplatform/baseframe/utils/JsonRedisSerializer.java
@@ -1,22 +1,22 @@
 package com.hq.cloudplatform.baseframe.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.hq.cloudplatform.baseframe.utils.json.JacksonUtil;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 /**
- * Created by Administrator on 7/24/2017.
+ * @author Administrator
+ * @date 7/24/2017
  */
 public class JsonRedisSerializer implements RedisSerializer<Object> {
 
     @Override
     public byte[] serialize(Object o) throws SerializationException {
-        return (null == o ? null : JSON.toJSONBytes(o, SerializerFeature.UseSingleQuotes));
+        return (null == o ? null : JacksonUtil.toJSONString(o).getBytes());
     }
 
     @Override
     public Object deserialize(byte[] bytes) throws SerializationException {
-        return (null == bytes ? null : JSON.parse(bytes));
+        return (null == bytes ? null : JacksonUtil.parseMap(new String(bytes)));
     }
 }

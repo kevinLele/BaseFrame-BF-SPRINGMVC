53ad5c2fac25147d19bd065493b3ad89f4331509
kevinLele <kevin.zhan@foxmail.com>
2018-04-16 21:23:21 +0800

    init

--- src/main/java/com/hq/cloudplatform/baseframe/exception/ServiceException.java
+++ src/main/java/com/hq/cloudplatform/baseframe/exception/ServiceException.java
@@ -4,7 +4,7 @@ package com.hq.cloudplatform.baseframe.exception;
  * 服务接口异常
  * @author Administrator
  */
-public class ServiceException extends Exception {
+public class ServiceException extends RuntimeException {
 
     private static final long serialVersionUID = 819257588855553664L;
 

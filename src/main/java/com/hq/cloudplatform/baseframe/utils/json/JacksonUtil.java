4baff792ba122db39c97fa7540907d76f57728e3
kevinLele <kevin.zhan@foxmail.com>
2018-04-17 16:14:00 +0800

    init

--- src/main/java/com/hq/cloudplatform/baseframe/utils/json/JacksonUtil.java
+++ src/main/java/com/hq/cloudplatform/baseframe/utils/json/JacksonUtil.java
@@ -1,8 +1,11 @@
 package com.hq.cloudplatform.baseframe.utils.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hq.cloudplatform.baseframe.exception.JsonParseException;
 import com.hq.cloudplatform.baseframe.restful.view.User;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +21,21 @@ import java.util.Map;
 @Slf4j
 public class JacksonUtil {
 
-    public static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper objectMapper;
+
+    public static ObjectMapper getObjectMapper() {
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+
+            //允许单引号
+            objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+
+            //忽略空属性
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        }
+
+        return objectMapper;
+    }
 
     /**
      * 使用泛型方法，把json字符串转换为相应的JavaBean对象。
@@ -31,17 +48,11 @@ public class JacksonUtil {
      * @return
      */
     public static <T> T parseObject(String jsonStr, Class<T> valueType) {
-        if (objectMapper == null) {
-            objectMapper = new ObjectMapper();
-        }
-
         try {
-            return objectMapper.readValue(jsonStr, valueType);
-        } catch (Exception e) {
-            log.error("Converting Json string to Object is error!", e);
+            return getObjectMapper().readValue(jsonStr, valueType);
+        } catch (IOException e) {
+            throw new JsonParseException(e);
         }
-
-        return null;
     }
 
     /**
@@ -53,39 +64,34 @@ public class JacksonUtil {
      * @return
      */
     public static <T> List<T> parseList(String jsonStr, Class<T> valueType) {
+        ObjectMapper objectMapper = getObjectMapper();
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(List.class, valueType);
+
         try {
-            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(List.class, valueType);
-
             return (List<T>) objectMapper.readValue(jsonStr, javaType);
-        } catch (Exception e) {
-            log.error("Converting Json string to Object list is error!", e);
+        } catch (IOException e) {
+            throw new JsonParseException(e);
         }
-
-        return null;
     }
 
     public static Map<String, Object> parseMap(String jsonStr) {
         try {
-            return objectMapper.readValue(jsonStr, Map.class);
+            return getObjectMapper().readValue(jsonStr, Map.class);
         } catch (IOException e) {
-            log.error("Converting Json string to Map is error!", e);
+            throw new JsonParseException(e);
         }
-
-        return null;
     }
 
     public static <T> Map<String, T> parseMap(String jsonStr, Class<T> valueType) {
+        ObjectMapper objectMapper = getObjectMapper();
         JavaType javaType = objectMapper.getTypeFactory().constructParametricType(Map.class, String.class, valueType);
 
         try {
             return objectMapper.readValue(jsonStr, javaType);
         } catch (IOException e) {
-            log.error("Converting Json string to Map is error!", e);
+            throw new JsonParseException(e);
         }
-
-        return null;
     }
-
 
     /**
      * 把JavaBean转换为json字符串
@@ -95,13 +101,10 @@ public class JacksonUtil {
      */
     public static String toJSONString(Object object) {
         try {
-            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-            return objectMapper.writeValueAsString(object);
-        } catch (Exception e) {
-            log.error("Converting Object to Json string is error!", e);
+            return getObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new JsonParseException(e);
         }
-
-        return null;
     }
 
     public static void main(String[] args) {
